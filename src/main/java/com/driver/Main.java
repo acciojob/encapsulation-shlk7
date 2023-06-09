466fd0b426fefb3e83eac99ada994d8267305d39
Shlok <theshlk77@gmail.com>
2023-06-09 21:13:53 +0530

    Created variable, getter and setters and printed the variable

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly rw=new RWOnly();
+//  rw.name="Shlok";
+//  System.out.println(rw.name);
+    rw.setName("Shlok");
+    System.out.print(rw.getName());
+  }
 }
